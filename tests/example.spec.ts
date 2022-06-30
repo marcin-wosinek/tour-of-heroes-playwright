c3984e9e62278c9017a6d3814b5356d044cbd495
Marcin Wosinek <marcin.wosinek@gmail.com>
2022-06-30 09:41:15 +0200

    add second test & stubs of the rest

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,4 +6,30 @@ test.describe('Tour of Heroes pages', () => {
 
     await expect(page).toHaveURL(/\/dashboard$/);
   });
+
+  test('should allow edits', async ({page}) => {
+    await page.goto('http://localhost:4200');
+
+    await page.locator('[data-test="detail-link"]').first().click();
+
+    await expect(page).toHaveURL(/\/detail\/\d+/);
+
+    await page.locator('#hero-name').fill('lorem ipsum');
+
+    await page.locator('[data-test="save-button"]').click();
+
+    await expect(page.locator('[data-test="detail-link"]').first()).toContainText('lorem ipsum');
+  });
+
+  test('should search for heroes', async ({page}) => {
+    await page.goto('http://localhost:4200');
+  })
+
+  test('should delete heroes', async ({page}) => {
+    await page.goto('http://localhost:4200/heroes');
+  })
+
+  test('should add heroes', async ({page}) => {
+    await page.goto('http://localhost:4200/heroes');
+  })
 });
