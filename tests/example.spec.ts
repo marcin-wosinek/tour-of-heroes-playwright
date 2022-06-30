c38d6d86df624f660b72e0d37f02d32c9afcdfec
Marcin Wosinek <marcin.wosinek@gmail.com>
2022-06-30 09:14:01 +0200

    implement first scenario

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,9 @@
 import { test, expect, type Page } from '@playwright/test';
 
-test.describe('Visit tour of heroes page', () => {
-  test('should load the page', async ({page}) => {
-    await page.goto('http://localhost:4200/heroes');
+test.describe('Tour of Heroes pages', () => {
+  test('should load default route', async ({page}) => {
+    await page.goto('http://localhost:4200');
 
-    await expect(page.locator('.heroes')).toContainText([
-      'Tornado'
-    ]);
+    await expect(page).toHaveURL(/\/dashboard$/);
   });
 });
