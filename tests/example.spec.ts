aa9cfb4155e5d679239e8eee94e83147ca700790
Marcin Wosinek <marcin.wosinek@gmail.com>
2022-07-11 18:08:31 +0200

    add basics test scenarios

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -23,13 +23,33 @@ test.describe('Tour of Heroes pages', () => {
 
   test('should search for heroes', async ({page}) => {
     await page.goto('http://localhost:4200');
+
+    await page.locator('#search-box').fill('nice');
+
+    await expect(page.locator('[data-test="search-result-link"]')).toHaveCount(1);
+
+    await page.locator('[data-test="search-result-link"]').click();
+
+    await expect(page).toHaveURL(/\/detail\/12/);
   })
 
   test('should delete heroes', async ({page}) => {
     await page.goto('http://localhost:4200/heroes');
+
+    await expect(page.locator('.delete')).toHaveCount(9);
+
+    page.locator('.delete').first().click();
+
+    await expect(page.locator('.delete')).toHaveCount(8);
   })
 
   test('should add heroes', async ({page}) => {
     await page.goto('http://localhost:4200/heroes');
+
+    await page.locator('#new-hero').fill('Lorem Ipsum');
+
+    page.locator('.add-button').click();
+
+    await expect(page.locator('.delete')).toHaveCount(10);
   })
 });
