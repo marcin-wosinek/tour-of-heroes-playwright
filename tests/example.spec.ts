e35c509ed5119160f812ac91f989d86b4e888622
Marcin Wosinek <marcin.wosinek@gmail.com>
2022-07-15 09:56:27 +0200

    Fix minor issue in tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -31,25 +31,25 @@ test.describe('Tour of Heroes pages', () => {
     await page.locator('[data-test="search-result-link"]').click();
 
     await expect(page).toHaveURL(/\/detail\/12/);
-  })
+  });
 
   test('should delete heroes', async ({page}) => {
     await page.goto('http://localhost:4200/heroes');
 
     await expect(page.locator('.delete')).toHaveCount(9);
 
-    page.locator('.delete').first().click();
+    await page.locator('.delete').first().click();
 
     await expect(page.locator('.delete')).toHaveCount(8);
-  })
+  });
 
   test('should add heroes', async ({page}) => {
     await page.goto('http://localhost:4200/heroes');
 
     await page.locator('#new-hero').fill('Lorem Ipsum');
 
-    page.locator('.add-button').click();
+    await page.locator('.add-button').click();
 
     await expect(page.locator('.delete')).toHaveCount(10);
-  })
+  });
 });
